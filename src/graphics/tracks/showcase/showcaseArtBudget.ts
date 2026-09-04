0251a7576f4d5923755e6d40548868f89b03c386
iLuzionsX <44279109+iLuzionsX@users.noreply.github.com>
2026-09-04 02:05:46 -0400

    art: add showcaseArtBudget.ts

---
+++ src/graphics/tracks/showcase/showcaseArtBudget.ts
@@ -0,0 +1,11 @@
+export const SHOWCASE_ART_BUDGET = {
+  targetTotalDrawCalls: 60,
+  estimatedBaselineDrawCalls: 20,
+  terrainDrawCalls: 7,
+  roadDetailDrawCalls: 7,
+  atmosphereDrawCalls: 2,
+  crowdDrawCalls: 4,
+  tracksideDrawCalls: 8,
+  maxKenneyVenueInstances: 12,
+  maxKenneyAssetIdsLoaded: 7,
+} as const;
